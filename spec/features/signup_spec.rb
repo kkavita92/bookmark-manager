b532f6991e4cded8cc1b220ebcd8540428021569
Kavita <kkavita92@gmail.com>
2017-06-02 14:50:55 +0100

    - Allows for password to be confirmed - Checks that when login form is submitted with incorrect password, no new users are created

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -4,4 +4,8 @@ feature 'sign up system for multiple users' do
     expect { sign_in }.to change(User, :count).by(1)
     expect(page).to have_content 'Welcome, kkavita92@gmail.com!'
   end
+
+  scenario 'no users created when wrong password input' do
+    expect { incorrect_sign_in }.not_to change(User, :count)
+  end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,7 +1,17 @@
 def sign_in
   visit('/users/new')
   expect(page.status_code).to eq(200)
-  fill_in('email', :with => 'kkavita92@gmail.com')
-  fill_in('password', :with => 'makers')
+  fill_in(:email, :with => 'kkavita92@gmail.com')
+  fill_in(:password, :with => 'makers')
+  fill_in(:password_confirmation, :with => 'makers')
+  click_button('Login')
+end
+
+def incorrect_sign_in
+  visit('/users/new')
+  expect(page.status_code).to eq(200)
+  fill_in(:email, :with => 'kkavita92@gmail.com')
+  fill_in(:password, :with => 'makers')
+  fill_in(:password_confirmation, :with => 'wrong')
   click_button('Login')
 end
